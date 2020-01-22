0014d135eadcf41de64651da1d432ba0c327c50a
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2020-01-21 21:10:03 -0500

    Update mycp.cpp

--- mycp.cpp
+++ mycp.cpp
@@ -1,39 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
  	ifstream fin;
  	ofstream fout;
-	
+
 	// check if there are enough arguments
-	
-	
-	// open the first file
- 	
-	char c;
+	if (argc == 3) {
+    string inFile = argv[1];
+    string outFile = argv[2];
 
- 	if (fin.fail()) // check if it is successful 
- 	{
- 		cerr << " Cannot open the input file!" << endl;
- 		return 1;
- 	}
- 	
+    // open the first file
+    fin.open(inFile);
+    char c;
 
-	// open the second file
-	
- 	if (fout.fail())
- 	{
- 		cerr << " Cannot open the output file!" << endl;
- 		return 1;
- 	}
- 	
- 	while(fin.get(c)) 
-	{
-		fout << c;
-	}
- 	
- 	fin.close(); 
+    if (fin.fail()) // check if it is successful
+    {
+      cerr << " Cannot open the input file!" << endl;
+      return 1;
+    }
 
- 	fout.close();
 
+    // open the second file
+    fout.open(outFile);
+
+    if (fout.fail())
+    {
+      cerr << " Cannot open the output file!" << endl;
+      return 1;
+    }
+
+    while(fin.get(c))
+    {
+      fout << c;
+    }
+
+    fin.close();
+
+    fout.close();
+
+  } else {
+    cerr << "Received an invalid number of command arguments! Expected 3,"
+              << " got " << argc << "." << '\n';
+  }
  	 return 0;
 
- } 
+ }
